97002964f861e4bc8371b029f9e7c19195febceb
Santiago Pozzolo <pozzolosanti@gmail.com>
2026-09-21 16:29:23 -0300

    Fix message for loaning book

--- src/main/java/com/biblioteca/model/Libro.java
+++ src/main/java/com/biblioteca/model/Libro.java
@@ -62,7 +62,7 @@ public class Libro {
 
     public String prestar() {
 
-        final result_negative = "No se puede prestar ahora"
+        final result_negative = "No se puede prestar";
 
         if (titulo == null) {
             return result_negative;
