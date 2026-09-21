873a106d55156ea4796b04555446a82a1147e8a3
Santiago Pozzolo <pozzolosanti@gmail.com>
2026-09-21 16:31:08 -0300

    Fix variable declaration syntax in prestar method

--- src/main/java/com/biblioteca/model/Libro.java
+++ src/main/java/com/biblioteca/model/Libro.java
@@ -62,7 +62,7 @@ public class Libro {
 
     public String prestar() {
 
-        final result_negative = "No se puede prestar";
+        final String result_negative = "No se puede prestar";
 
         if (titulo == null) {
             return result_negative;
